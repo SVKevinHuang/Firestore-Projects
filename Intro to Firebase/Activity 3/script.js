230abc344ba1d0a0ecabccbd307a69bc55082318
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2020-11-19 22:22:10 -0500

    finished

---
+++ Intro to Firebase/Activity 3/script.js
@@ -0,0 +1,24 @@
+// All firebase use needs to be after the DOM has loaded, to make sure that firebase itself has been loaded
+document.addEventListener("DOMContentLoaded", event => {
+  const app = firebase.app();
+    
+  console.log(app); //Just to make sure it's working
+
+  const db = firebase.firestore();
+  const todoRef = db.collection('todos');
+  const todoList = document.getElementById("todos");
+
+  todoRef.onSnapshot(todos => {
+
+    //To clear the items wrote from previous updates
+    while (todoList.firstChild)
+      todoList.removeChild(todoList.firstChild);
+
+    todos.forEach(doc => {
+      data = doc.data();
+      li = document.createElement('li');
+      li.textContent = data.title;
+      todoList.appendChild(li);
+    })
+  })
+})

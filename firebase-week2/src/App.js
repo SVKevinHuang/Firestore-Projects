925db3700f2c86f9b45256a9bb7f75eb36f39c0c
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2021-01-08 18:54:14 -0500

    commit

--- firebase-week2/src/App.js
+++ firebase-week2/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import firebase from 'firebase/app'
 import 'firebase/firestore'
@@ -20,29 +20,38 @@ if (!firebase.apps.length) {
 }
 
 function App() {
-  const [firebaseText, setFirebaseText] = useState("");
+  const [todos, setTodos] = useState([]);
   const [inputValue, setInputValue] = useState("");
 
-
-  firebase.firestore().collection("memo").doc("test").get().then(doc => {
-    console.log(doc.data());
-    setFirebaseText(doc.data().text);
-  })
+  useEffect(()=>{
+    firebase.firestore().collection("todos").get().then(snapshot => {
+      const newTodos = [];
+      snapshot.docs.map((doc) => {
+        newTodos.push(doc.data().title);
+      })
+      console.log(newTodos);
+      setTodos(newTodos);
+    })
+    console.log("useEffect called");
+  }, []);
+  //return [] so useEffect is only called on the initial render
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          {firebaseText}
-        </p>
-        <form onSubmit = {(e) => {
+        <ul>
+        {todos.map((todo) => {
+          return( <li> {todo} </li>);
+        })}
+        </ul>
+        <form onSubmit={(e) => {
           //Puts data on firebase
           e.preventDefault();
-          firebase.firestore().collection("memo").doc("test").set({
-            text: inputValue
+          firebase.firestore().collection("todos").add({
+            title: inputValue
           })
         }}>
-          <input value={inputValue} onChange={(e) => {setInputValue(e.target.value);}}/>
+          <input value={inputValue} onChange={(e) => { setInputValue(e.target.value); }} />
         </form>
       </header>
     </div>
